398d77e6c963473bec81fad48a44ea57e8bef2d3
Enaldo Narvaez Yepes <enaldo_17@hotmail.com>
2023-01-15 02:42:36 -0500

    feat: Add help prompt

--- src/main.go
+++ src/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"os"
+)
+
+const (
+	helpPrompt = `Usage: colorconverter [-h | --help] <command> [-i [<color format>]] [-o [<color format>]] color
+
+There are the supported commands:
+ convert  Used to covert an input color format into the specified color format:
+          Eg. colorconverter convert -i hex -o rgb '#5fafff'
+              rgb(95,175,255)
+	
+ info     Used to show all conversions on th supported color formats:
+          Eg. colorconverter all -i hex '#5fafff'
+              Color conversions:
+              Hex     #5FAFFF
+              RGB     rgb(95,175,255)
+              X11     Recommended closest value: 75
+
+Supported color formats:
+  hex 
+  rgb
+  x11
+`
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	args := os.Args
+
+	if args[1] == "-h" || args[1] == "--help" {
+		showHelp()
+	}
+
+}
+
+func showHelp() {
+	fmt.Print(helpPrompt)
 }
